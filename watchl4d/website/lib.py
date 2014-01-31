29fe13b9edeb0709b297c157a4112adb29ad21a1
Chad Harter <chad.harter@budgettravel.com>
2014-01-31 18:44:25 -0500

    added estoopi channel

--- watchl4d/website/lib.py
+++ watchl4d/website/lib.py
@@ -5,6 +5,7 @@ from django.core.cache import cache
 
 # These are listed in priority
 CHANNELS = [
+{'name': 'estoopi', 'provider': 'twitch'},
 {'name': 'babycakez_', 'provider': 'twitch'},
 {'name': 'dexterity_', 'provider': 'twitch'},
 {'name': 'usandy', 'provider': 'twitch'},

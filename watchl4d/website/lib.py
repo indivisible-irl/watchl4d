d49bee46991f9a206ff7dc8e27a273c3f23f7ddf
Chad Harter <chad.harter@budgettravel.com>
2014-02-04 15:51:19 -0500

    Added cabisan04

--- watchl4d/website/lib.py
+++ watchl4d/website/lib.py
@@ -5,6 +5,7 @@ from django.core.cache import cache
 
 # These are listed in priority
 CHANNELS = [
+{'name': 'cabisan04', 'provider': 'twitch'},
 {'name': 'yesmrpresident', 'provider': 'twitch'},
 {'name': 'estoopi', 'provider': 'twitch'},
 {'name': 'babycakez_', 'provider': 'twitch'},
